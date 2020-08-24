eb01228737a971ab23505638286393a6e32cbe2d
fog <jazz.fog@gmail.com>
2020-08-24 12:30:30 -0700

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactNode } from 'react';
+
+// TypeScript 4 adds incorrect dependencies (TypeScript as opposed to compiled code)
+
+// Correct imports, added manually
+// ---
+// import { useFieldArray, useForm } from "react-hook-form";
+
+// Wrong imports (added by TypeScript 4 and VSCode)
+// This won't work in prod-ish Node environment once the project is compiled since the dependencies 
+// are pointing at the not-compiled TypeScript as opposed JS that Node could run.
+// ---
+// import { useFieldArray } from 'react-hook-form/dist/useFieldArray';
+// import { useForm } from 'react-hook-form/dist/useForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  // Press `Ctrl + .` (Windows) or `Cmd + .` (macOS)
+  // 
+  // console.log(useForm, useFieldArray);
+
+  return <>...</>;
 }
 
 export default App;
